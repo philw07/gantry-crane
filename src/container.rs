5baa31cbd6abb25a2d615f598af2e8cab5b21265
Philipp W <phil.w07@gmail.com>
2022-09-08 22:46:00 +0200

    Add unit tests for container

--- src/container.rs
+++ src/container.rs
@@ -132,3 +132,252 @@ where
 {
     s.serialize_str(&name[1..])
 }
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+
+    use bollard::{
+        container::{
+            BlkioStats, CPUStats, CPUUsage, MemoryStats, PidsStats, Stats, StorageStats,
+            ThrottlingData,
+        },
+        service::{
+            ContainerInspectResponse, ContainerState, ContainerStateStatusEnum, Health,
+            HealthStatusEnum,
+        },
+    };
+
+    use crate::{constants::UNKNOWN, mqtt::MqttClient, settings::Settings};
+
+    use super::Container;
+
+    #[test]
+    fn test_new_container() {
+        let name = "test_name";
+        let image = "test-image";
+        let mqtt = Rc::new(MqttClient::new(&Settings::new()).unwrap());
+
+        let stats = get_stats(name);
+        let inspect = ContainerInspectResponse {
+            name: Some(name.into()),
+            ..Default::default()
+        };
+
+        let container = Container::new(mqtt.clone(), stats, inspect, Some(image.into()));
+
+        assert_eq!(container.name, name);
+        assert_eq!(container.image, image);
+        assert_eq!(container.last_published, None);
+        assert!(Rc::ptr_eq(&container.mqtt, &mqtt));
+    }
+
+    #[test]
+    fn test_new_container_no_image() {
+        let name = "other_name";
+        let mqtt = Rc::new(MqttClient::new(&Settings::new()).unwrap());
+
+        let stats = get_stats(name);
+        let inspect = ContainerInspectResponse {
+            name: Some(name.into()),
+            ..Default::default()
+        };
+
+        let container = Container::new(mqtt.clone(), stats, inspect, None);
+
+        assert_eq!(container.name, name);
+        assert_eq!(container.image, UNKNOWN.to_owned());
+        assert_eq!(container.last_published, None);
+        assert!(Rc::ptr_eq(&container.mqtt, &mqtt));
+    }
+
+    #[test]
+    fn test_rename() {
+        let name = "original_name";
+        let mqtt = Rc::new(MqttClient::new(&Settings::new()).unwrap());
+
+        let stats = get_stats(name);
+        let inspect = ContainerInspectResponse {
+            name: Some(name.into()),
+            ..Default::default()
+        };
+
+        let mut container = Container::new(mqtt.clone(), stats, inspect, None);
+        assert_eq!(container.name, name);
+
+        let new_name = "renamed_name";
+        container.rename(new_name.into());
+        assert_eq!(container.name, new_name);
+    }
+
+    #[test]
+    fn test_update() {
+        let mqtt = Rc::new(MqttClient::new(&Settings::new()).unwrap());
+        let stats = get_stats("");
+        let mut inspect = ContainerInspectResponse {
+            ..Default::default()
+        };
+
+        let mut container = Container::new(mqtt.clone(), stats.clone(), inspect.clone(), None);
+
+        let status = ContainerStateStatusEnum::DEAD;
+        let health = HealthStatusEnum::UNHEALTHY;
+        inspect.state = Some(ContainerState {
+            status: Some(status),
+            health: Some(Health {
+                status: Some(health),
+                ..Default::default()
+            }),
+            ..Default::default()
+        });
+        container.update(stats, inspect);
+        assert_eq!(container.state, status.as_ref());
+        assert_eq!(container.health, health.as_ref());
+    }
+
+    #[test]
+    fn test_parse_state() {
+        let mut inspect = ContainerInspectResponse {
+            ..Default::default()
+        };
+        assert_eq!(Container::parse_state(&inspect), UNKNOWN);
+
+        // Test no status
+        inspect.state = Some(ContainerState {
+            ..Default::default()
+        });
+        assert_eq!(Container::parse_state(&inspect), UNKNOWN);
+
+        // Test each status value
+        let enum_values = [
+            ContainerStateStatusEnum::EMPTY,
+            ContainerStateStatusEnum::CREATED,
+            ContainerStateStatusEnum::DEAD,
+            ContainerStateStatusEnum::EXITED,
+            ContainerStateStatusEnum::PAUSED,
+            ContainerStateStatusEnum::REMOVING,
+            ContainerStateStatusEnum::RESTARTING,
+            ContainerStateStatusEnum::RUNNING,
+        ];
+        for val in enum_values {
+            inspect.state = Some(ContainerState {
+                status: Some(val),
+                ..Default::default()
+            });
+            let expected = if val == ContainerStateStatusEnum::EMPTY {
+                UNKNOWN
+            } else {
+                val.as_ref()
+            };
+            assert_eq!(Container::parse_state(&inspect), expected);
+        }
+    }
+
+    #[test]
+    fn test_parse_health() {
+        let mut inspect = ContainerInspectResponse {
+            ..Default::default()
+        };
+        assert_eq!(Container::parse_health(&inspect), UNKNOWN);
+
+        // Test no health
+        inspect.state = Some(ContainerState {
+            ..Default::default()
+        });
+        assert_eq!(Container::parse_state(&inspect), UNKNOWN);
+
+        // Test no health status
+        inspect.state = Some(ContainerState {
+            health: Some(Health {
+                ..Default::default()
+            }),
+            ..Default::default()
+        });
+        assert_eq!(Container::parse_state(&inspect), UNKNOWN);
+
+        // Test each health status
+        let enum_values = [
+            HealthStatusEnum::EMPTY,
+            HealthStatusEnum::HEALTHY,
+            HealthStatusEnum::NONE,
+            HealthStatusEnum::STARTING,
+            HealthStatusEnum::UNHEALTHY,
+        ];
+        for val in enum_values {
+            inspect.state = Some(ContainerState {
+                health: Some(Health {
+                    status: Some(val),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            });
+            let expected = if val == HealthStatusEnum::EMPTY {
+                UNKNOWN
+            } else {
+                val.as_ref()
+            };
+            assert_eq!(Container::parse_health(&inspect), expected);
+        }
+    }
+
+    fn get_stats(name: &str) -> Stats {
+        let cpu = CPUStats {
+            cpu_usage: CPUUsage {
+                percpu_usage: None,
+                total_usage: 1,
+                usage_in_kernelmode: 1,
+                usage_in_usermode: 1,
+            },
+            online_cpus: None,
+            system_cpu_usage: None,
+            throttling_data: ThrottlingData {
+                periods: 1,
+                throttled_periods: 1,
+                throttled_time: 1,
+            },
+        };
+        Stats {
+            id: "".into(),
+            read: "".into(),
+            preread: "".into(),
+            num_procs: 1,
+            pids_stats: PidsStats {
+                current: None,
+                limit: None,
+            },
+            network: None,
+            blkio_stats: BlkioStats {
+                io_merged_recursive: None,
+                io_queue_recursive: None,
+                io_service_bytes_recursive: None,
+                io_service_time_recursive: None,
+                io_serviced_recursive: None,
+                io_time_recursive: None,
+                io_wait_time_recursive: None,
+                sectors_recursive: None,
+            },
+            cpu_stats: cpu.clone(),
+            memory_stats: MemoryStats {
+                commit: None,
+                commit_peak: None,
+                commitbytes: None,
+                commitpeakbytes: None,
+                failcnt: None,
+                limit: None,
+                max_usage: None,
+                privateworkingset: None,
+                stats: None,
+                usage: None,
+            },
+            name: name.into(),
+            networks: None,
+            precpu_stats: cpu,
+            storage_stats: StorageStats {
+                read_count_normalized: None,
+                read_size_bytes: None,
+                write_count_normalized: None,
+                write_size_bytes: None,
+            },
+        }
+    }
+}
